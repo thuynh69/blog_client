d6047871479ceb8213ace3a430ee465229f94168
Valentin <vchantelot@turbulent.ca>
2022-09-29 22:01:08 +0200

    init(client) - Add SASS support and .env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "variables.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

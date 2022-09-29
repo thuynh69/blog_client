e91561548a7a618fa059d487776d5fc1f7802e62
Valentin <vchantelot@turbulent.ca>
2022-09-29 19:46:31 +0200

    init(client) - Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  semi: true,
+  arrowParens: 'always',
+};
